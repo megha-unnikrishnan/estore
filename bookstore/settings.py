4bc546b7412b8efa682d9af1f9cab16dbde66693
Megha Unnikrishnan <129495089+megha-unnikrishnan@users.noreply.github.com>
2024-05-11 00:36:43 +0530

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.53.175.122','localhost']
 
 # Application definition
 
