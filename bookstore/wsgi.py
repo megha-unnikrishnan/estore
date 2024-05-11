94e24ff7e4a6c5ae44771494e2cb2ef46fbcf857
Megha Unnikrishnan <129495089+megha-unnikrishnan@users.noreply.github.com>
2024-05-11 12:55:48 +0530

    Update wsgi.py

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
 
+
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
