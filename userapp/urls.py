bb5a8ad1a16b3476899088b2f7d6c42c615dd9c8
Megha Unnikrishnan <129495089+megha-unnikrishnan@users.noreply.github.com>
2024-05-13 00:27:24 +0530

    Update urls.py

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-    path('index/',views.index,name='userindex'),
+    path('',views.index,name='userindex'),
     path('login/',views.user_login,name='login'),
     path('register/',views.user_signup,name='register'),
     path('logout/',views.user_logout,name='logout'),
